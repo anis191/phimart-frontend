dd9d9af0ca502f38439314b9681ab098b1ce947d
anisul alam <anisulalam2002@gmail.com>
2025-08-04 21:47:07 +0600

    feat: complete cart functionality

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -5,8 +5,10 @@ import authApiClient from "../services/auth-api-client";
 const useCart = () => {
     const[cart, setCart] = useState(null)
     const[cartId, setCartId] = useState(() => localStorage.getItem('cartId'))
+    const[loading, setLoading] = useState(false)
 
     const createOrGetCart = useCallback(async () =>{
+        setLoading(true)
         try{
             const response = await authApiClient.post("/carts/")
             if (!cartId){
@@ -14,21 +16,39 @@ const useCart = () => {
                 setCartId(response.data.id)
             }
             setCart(response.data)
-            console.log(response.data)
+            // console.log(response.data)
         }catch(error){console.log(error)}
+        finally{setLoading(false)}
     },[cartId])
 
     // Add items to the cart:
     const addCartItems = useCallback(async(data) =>{
+        setLoading(true)
         if(!cartId) await createOrGetCart();
         try{
             const response = await authApiClient.post(`/carts/${cartId}/items/`,data)
-            console.log("From useCart hook: ",response.data)
+            // console.log("From useCart hook: ",response.data)
             return response.data
         }catch(error){console.log(error)}
+        finally{setLoading(false)}
     },[cartId,createOrGetCart])
 
-    return { createOrGetCart, cart, addCartItems }
+    // Update cart item quantity:
+    const updateCartItemQuantity = useCallback(async(itemId, newQuantity) => {
+        try{
+            const response = await authApiClient.patch(`/carts/${cartId}/items/${itemId}/`,{quantity:newQuantity})
+            console.log(response)
+        }catch(error){console.log(error)}
+    },[cartId])
+
+    // Delete Cart Items:
+    const deleteCartItems = useCallback(async(itemId) =>{
+        try{
+            await authApiClient.delete(`/carts/${cartId}/items/${itemId}/`)
+        }catch(error){console.log(error)}
+    },[cartId])
+
+    return { createOrGetCart, cart, addCartItems, updateCartItemQuantity, loading, deleteCartItems }
 };
 
 export default useCart;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,16 +1,102 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useCartContext from "../hooks/useCartContext";
+import CartItemList from "../components/Cart/CartItemList";
+import CartSummary from "../components/Cart/CartSummary";
 
 const Cart = () => {
-    const {createOrGetCart, cart} = useCartContext()
+    const {createOrGetCart, updateCartItemQuantity, cart, loading, deleteCartItems} = useCartContext()
+    const [localCart, setLocalCart] = useState(cart)
     
     useEffect(()=>{
         createOrGetCart()
     },[createOrGetCart])
+
+    useEffect(()=>{
+        setLocalCart(cart)
+    },[cart])
     
+    // const handleUpdateQuantity = async(itemId, quantity) => {
+        // const prevLocalCartCopy = localCart // store a capy of localCart
+// 
+        // setLocalCart((prevLocalCart) => (
+            // {... prevLocalCart, items: prevLocalCart.items.map((item)=> item.id === itemId ? {
+                // ... item, quantity: quantity, total_price: item.product.price * quantity,
+            // } : item),
+            // total_price: prevLocalCart.items.reduce(
+                // (sum, item) => sum + item.total_price, 0
+            // )
+            // }
+        // ))
+        // try{
+            // await updateCartItemQuantity(itemId, quantity)
+        // }catch(error){
+            // console.log(error)
+            // setLocalCart(prevLocalCartCopy)
+        // }
+    // }
+
+    const handleUpdateQuantity = async(itemId, quantity) => {
+        const prevLocalCartCopy = localCart // store a capy of localCart
+
+        setLocalCart((prevLocalCart) => {
+            const updateItems = prevLocalCart.items.map((item)=>
+                item.id === itemId ? {
+                    ... item,
+                    quantity: quantity, total_price: item.product.price * quantity,
+                } : item
+            )
+            
+            return {
+                ... prevLocalCart,
+                items: updateItems,
+                total_price: updateItems.reduce(
+                (sum, item) => sum + item.total_price, 0
+            )
+            }
+        })
+        try{
+            await updateCartItemQuantity(itemId, quantity)
+        }catch(error){
+            console.log(error)
+            setLocalCart(prevLocalCartCopy)
+        }
+    }
+
+    // const handleRemoveItem = async(itemId) =>{
+        // setLocalCart((prevLocalCart)=>({
+            // ... prevLocalCart, 
+            // items: prevLocalCart.items.filter((item) => item.id !== itemId),
+        // }))
+// 
+        // try{
+            // await deleteCartItems(itemId)
+        // }catch(error){console.log(error)}
+    // }
+
+    const handleRemoveItem = async(itemId) =>{
+        setLocalCart((prevLocalCart) => {
+            const updatedItems = prevLocalCart.items.filter((item) => item.id !== itemId)
+
+            return {
+            ... prevLocalCart, 
+            items: updatedItems,
+            total_price: updatedItems.reduce((sum, item) => sum + item.total_price, 0)
+        }})
+
+        try{
+            await deleteCartItems(itemId)
+        }catch(error){console.log(error)}
+    }
+
+    if(loading) return <p className="text-blue-500 text-center p-5 font-bold">Loading....</p>
+    if(!localCart) return <p className="text-blue-500 text-center p-5 font-bold">No cart found</p>
+
     return (
-        <div className="bg-red-400 p-5">
-            {JSON.stringify(cart)}
+        <div className="container mx-auto px-4 py-8">
+            <div className="grid grid-cols-1 md:grid-cols-2">
+                <div><CartItemList items={localCart.items} handleUpdateQuantity={handleUpdateQuantity} handleRemoveItem={handleRemoveItem}/></div>
+                <div><CartSummary totalPrice={localCart.total_price} itemCount={localCart.items.length}/></div>
+            </div>
         </div>
     );
 };
